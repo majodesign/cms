3e2f6bcd21e26679fb0e259af70d9472fffd4356
Majodesign <support@majodesign.nl>
2011-06-22 00:52:18 +0200

    Adding project CMS

---
+++ application/models/site_model.php
@@ -0,0 +1,29 @@
+<?php
+
+class Site_model extends Model
+{
+	
+	function get_records()
+	{
+		$query = $this->db->get('data');
+		return $query->result();
+	}
+	
+	function add_record($data)
+	{
+		$this->db->insert('data', $data);
+		return;
+	}
+	
+	function update_record($data)
+	{
+		$this->db->where('id', $this->uri->segment(3));
+		$this->db->update('data', $data);
+	}
+	
+	function delete_row()
+	{
+		$this->db->where('id', $this->uri->segment(3));
+		$this->db->delete('data');
+	}
+}
